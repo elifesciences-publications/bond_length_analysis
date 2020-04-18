c0d734634c57a7b2a58a1b9c5328645e1a21ff7e
David <david.sauer@med.nyu.edu>
2020-04-18 16:44:29 -0400

    modify for neater inputs and to output individual results

--- backbone_calc.py
+++ backbone_calc.py
@@ -5,19 +5,27 @@ from sys import argv
 import os
 import multiprocessing as mp
 import statistics
+import argparse
 
 AAs = ['ALA','CYS','ASP','GLU','PHE','GLY','HIS','ILE','LYS','LEU','MET','ASN','PRO','GLN','ARG','SER','THR','VAL','TRP','TYR']
 
-atom1 = 'CA'
-atom2 = 'C'
-name = argv[1]
-directory = argv[2]
-threads = int(argv[3])
+parser = argparse.ArgumentParser(description='Calculate the Ca-C bond lengths of protein structures')
+parms = parser.add_argument_group('Required parameters')
+parms.add_argument("-d","--directory",action='store', type=str, help="The directory of the PDB files in .pdb or .cif format.",dest='directory',default='')
+parms.add_argument("-n","--name",action='store', type=str, help="The name to give the summary file.",dest='name',default='')
+parms.add_argument("-t", "--threads",action='store', type=int, help="Number of threads to run in parallel. Default is 1.",dest='threads',default=1)
+parms.add_argument("-i", "--individual",action='store_true', help="Save a list of Ca-C distances for each structure.",dest='indiv',default=False)
+args = parser.parse_args()
+
+name = args.name
+directory = args.directory
+threads = args.threads
+indiv = args.indiv
 
 def distance(position):
 	try:
-		pos_a = position[atom1].get_coord()
-		pos_b = position[atom2].get_coord()
+		pos_a = position['CA'].get_coord()
+		pos_b = position['C'].get_coord()
 	except:
 		return np.nan
 	else:
@@ -40,7 +48,10 @@ def calc(name):
 	dist_1 = [distance(residue) for structs in structure for chain in structs for residue in chain if residue.get_resname() in AAs]
 	mean = statistics.mean(dist_1)
 	median = statistics.median(dist_1)
-	return {'name':'.'.join(name.split('/')[-1].split('.')[:-1]),'mean':mean,'method':method,'date':date,'resolution':resolution,'median':median}
+	if indiv:
+		return {'name':'.'.join(name.split('/')[-1].split('.')[:-1]),'mean':mean,'method':method,'date':date,'resolution':resolution,'median':median,'list':dist_1}
+	else:
+		return {'name':'.'.join(name.split('/')[-1].split('.')[:-1]),'mean':mean,'method':method,'date':date,'resolution':resolution,'median':median}
 
 def runner(model):
 	#return calc(model)
@@ -64,3 +75,8 @@ with open(name+'_Ca-C_distances.tsv','w') as f:
 	for distance in distances:
 		if not(distance == None):
 			f.write(distance['name']+'\t'+str(distance['date'])+'\t'+str(distance['method'])+'\t'+str(distance['resolution'])+'\t'+str(distance['mean'])+'\t'+str(distance['median'])+'\n')
+
+if indiv:
+	for distance in distances:
+		with open(distance['name']+'_Ca-C_distances.txt','w') as f:
+			f.write('\n'.join([str(x) for x in distance['list']]))
