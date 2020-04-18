d73ee1003ba85565e46a14ea953ebe344108c63d
david <david.sauer@med.nyu.edu>
2020-04-18 01:28:07 -0400

    modify for neater file names

--- ideal_calculator.bash
+++ ideal_calculator.bash
@@ -1,7 +1,6 @@
 #/usr/bin/bash
 
-wget http://kinemage.biochem.duke.edu/php/downlode.php?filename=/downloads/datasets/top500H.tgz
-
+wget http://kinemage.biochem.duke.edu/php/downlode.php?filename=/downloads/datasets/top500H.tgz -O top500H.tgz
 tar -zxvf top500H.tgz
 for f in ./top500H/*
 do 
@@ -9,7 +8,6 @@ do
 done
 python3 backbone_calc.py ./top500H/ 4
 
-wget http://kinemage.biochem.duke.edu/php/downlodekins.php?filename=/downloads/datasets/top8000_chains_70.tgz
-
+wget http://kinemage.biochem.duke.edu/php/downlodekins.php?filename=/downloads/datasets/top8000_chains_70.tgz -O top8000_chains_70.tgz
 tar -zxvf top8000_chains_70.tgz
 python3 backbone_calc.py ./top8000_chains_70/ 4
