094a5eb81c46f3d3a9c060f140b5d1861be34d92
david <david.sauer@med.nyu.edu>
2020-04-18 02:05:49 -0400

    updates

--- pdb_analysis.bash
+++ pdb_analysis.bash
@@ -2,14 +2,14 @@
 
 mkdir cryoem/
 cd cryoem/
-sed -e 's/^/https:\/\/files.rcsb.org\/download\//; s/$/.pdb/' ../PDB_cryoem.tsv > ../PDB_cryoem_mod.tsv
+sed -e 's/^/https:\/\/files.rcsb.org\/download\// ; s/$/.pdb/' ../PDB_cryoem.tsv > ../PDB_cryoem_mod.tsv
 wget -i ../PDB_cryoem_mod.tsv
 cd ..
 python3 backbone_calc.py cryoem ./cryoem/ $1
 
 mkdir xray/
 cd xray/
-sed -e 's/^/https:\/\/files.rcsb.org\/download\//; s/$/.pdb/' ../PDB_cryoem.tsv > ../PDB_cryoem_mod.tsv
+sed -e 's/^/https:\/\/files.rcsb.org\/download\//; s/$/ .pdb/' ../PDB_cryoem.tsv > ../PDB_cryoem_mod.tsv
 wget -i ../PDB_cryoem_mod.tsv
 cd ..
 python3 backbone_calc.py xray ./xray/ $1
